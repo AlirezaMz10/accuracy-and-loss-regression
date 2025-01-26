1d71d2c78975e376e52bb7273ab678817a0f50bc
alirezamz <alirezapple@gmail.com>
2025-01-26 13:13:51 +0330

    edit

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 x_data = np.array([2, 2.5, 2.5, 3, 3.2, 3.5, 4, 4, 4, 4.5, 4.7, 4.8, 5, 5, 5.5])
-x_data
-
 y_data = np.array([5, 5, 6.5, 6, 7, 7.5, 7.5, 7, 8.5, 8, 8, 8.5, 10, 10.5, 11])
-y_data
-
 
 class LinearRegression:
     weights = []
@@ -29,17 +25,15 @@ class LinearRegression:
         predicts = np.array(list(map(lambda s: np.float64(format(s, ".2f")), predicts)))
         return predicts
     
-
 l1 = LinearRegression()
 l1.fit(x_data, y_data)
 y_hats = l1.predict(x_data)
-y_hats
 
-mae = np.float64(format(sum(abs(y_data - y_hats)) / len(y_data), ".2f"))
-mae
-
-mse = np.float64(format(sum(abs(y_data - y_hats)) / len(y_data), ".2f"))
-mse
-
+mae = np.float64(format(sum(abs(y_data - y_hats)) /len(y_data), ".2f"))
+mse = np.float64(format(sum(((y_data - y_hats) ** 2)) / len(y_data), ".2f"))
+y_bar = np.float64(format(np.mean(y_data), ".2f"))
 r2_score = np.float64(format(1 - (sum(((y_data - y_hats) ** 2))) / sum(((y_data - y_bar) ** 2)), ".2f"))
-r2_score
+
+print(mae)
+print(mse)
+print(r2_score)
